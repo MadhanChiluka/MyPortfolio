9e5504c7260c732a0e3bb02047b99969807418c5
MadhanChiluka <30656287+MadhanChiluka@users.noreply.github.com>
2019-02-19 22:31:24 +0530

    Update portfolio.data.ts

--- src/app/utils/data/portfolio.data.ts
+++ src/app/utils/data/portfolio.data.ts
@@ -6,8 +6,8 @@ export const portfoliloData: IProject = {
   description:
     'This is my portfolio website (SPA), designed with Adobe XD & developed with Angular 5',
   isFeatured: true,
-  projectLink: 'https://github.com/ritwickdey/ritwickdey.github.io',
-  liveUrl: 'https://ritwickdey.github.io',
+  projectLink: 'https://github.com/MadhanChiluka/MyPortfolio',
+  liveUrl: 'https://madhanchiluka.github.io/MyPortfolio/',
   features: [
     'No 3rd party CSS library or component is used 💖',
     'Filtering Project Cards in Portfolio Page',
